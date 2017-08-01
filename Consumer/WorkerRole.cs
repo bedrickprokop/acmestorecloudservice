baad9723b316845031ee047d210a23d0436b4be3
Bedrick <bedrick.prokop@gmail.com>
2017-08-01 03:00:28 -0300

    Adicionar arquivos de projeto.

---
+++ Consumer/WorkerRole.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+
+namespace Consumer
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        private readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
+
+        public override void Run()
+        {
+            Trace.TraceInformation("Consumer is running");
+
+            try
+            {
+                this.RunAsync(this.cancellationTokenSource.Token).Wait();
+            }
+            finally
+            {
+                this.runCompleteEvent.Set();
+            }
+        }
+
+        public override bool OnStart()
+        {
+            // Definir o número máximo de conexões simultâneas
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // Para obter informações sobre como tratar as alterações de configuração
+            // veja o tópico do MSDN em https://go.microsoft.com/fwlink/?LinkId=166357.
+
+            bool result = base.OnStart();
+
+            Trace.TraceInformation("Consumer has been started");
+
+            return result;
+        }
+
+        public override void OnStop()
+        {
+            Trace.TraceInformation("Consumer is stopping");
+
+            this.cancellationTokenSource.Cancel();
+            this.runCompleteEvent.WaitOne();
+
+            base.OnStop();
+
+            Trace.TraceInformation("Consumer has stopped");
+        }
+
+        private async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: substitua o item a seguir pela sua própria lógica.
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Trace.TraceInformation("Working");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
